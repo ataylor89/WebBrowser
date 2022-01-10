3e2eeea06b59a3d608558112680744587495e174
Andrew Taylor <andrewtaylor@Andrews-MacBook-Pro.local>
2022-01-09 21:00:23 -0600

    Committing code for Web Browser

---
+++ src/main/java/webbrowser/WebBrowser.java
@@ -0,0 +1,30 @@
+package webbrowser;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebView;
+import javafx.scene.web.WebEngine;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author andrewtaylor
+ */
+public class WebBrowser extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        WebView browser = new WebView();
+        WebEngine webEngine = browser.getEngine();
+        webEngine.load("http://www.oracle.com");
+        Scene scene = new Scene(browser, 1200, 900);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
